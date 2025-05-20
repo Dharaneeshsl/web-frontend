d5ac4cca21d0d6e246e231977b0a3ea6e0ff01cc
SHALMAN <shalman4502n@gmail.com>
2025-05-20 22:08:45 +0530

    removed lenis

--- src/history.jsx
+++ src/history.jsx
@@ -3,18 +3,11 @@ import "./history.css";
 import Navbar from "./navbar";
 import ScrollCards from "./scrollcards";
 import { useEffect } from "react";
-import Lenis from "lenis";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 function History() {
-   useEffect(() => {const lenis = new Lenis();
-
-      lenis.on("scroll", ScrollTrigger.update);
-      gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-      });
-         gsap.ticker.lagSmoothing(0);},  []);
+  
     return (
        <> 
        <Navbar></Navbar>
